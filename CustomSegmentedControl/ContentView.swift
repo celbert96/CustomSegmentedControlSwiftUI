29e62a2fb122c5ce08362de1e5467baf15b080ab
Chris Elbert <celbert96@gmail.com>
2024-02-06 17:02:15 -0500

    Initial Commit

---
+++ CustomSegmentedControl/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CustomSegmentedControl
+//
+//  Created by Christopher Elbert on 2/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CustomSegmentedControl/CustomSegmentedControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomSegmentedControlApp.swift
+//  CustomSegmentedControl
+//
+//  Created by Christopher Elbert on 2/6/24.
+//
+
+import SwiftUI
+
+@main
+struct CustomSegmentedControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
